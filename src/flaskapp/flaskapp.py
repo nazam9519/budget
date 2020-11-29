0e8ccff55a8970e845a703829dbd9534cd480c10
Nabil Azam <na9519@outlook.com>
2020-11-29 15:38:23 -0500

    added some new code for db

--- src/flaskapp/flaskapp.py
+++ src/flaskapp/flaskapp.py
@@ -1,11 +1,17 @@
 from flask import Flask
-from square import getsquare
+from flask_sqlalchemy import SQLAlchemy
+from src.flaskapp.square import getsquare
+from src.flaskapp.model import dbconn
 
 #main python flask handler
 #calls a blueprint
 
 app = Flask(__name__)
 app.register_blueprint(getsquare)
+app.register_blueprint(model)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
